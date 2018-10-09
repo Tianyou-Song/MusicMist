5fedf1b70623f9d34d9f66cc577a61ff6982f672
DawgSlaya <tim.d.song@gmail.com>
2018-10-09 16:47:25 -0700

    minor changes

--- frontend/music-mist.jsx
+++ frontend/music-mist.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from '/store/store.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
   ReactDOM.render(<Root>Hello from react</Root>, root);
 });
