f83b3783b24fc3c82fc2d4e6f9fac94575c3a925
DawgSlaya <tim.d.song@gmail.com>
2018-10-09 16:13:33 -0700

    configure jackage.json

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           query: {
-            presets: ['env', 'react']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         },
       }
