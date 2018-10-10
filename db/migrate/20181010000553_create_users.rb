d7cc9f5efe21d0edaac0cbc1ce70d3b920d13389
DawgSlaya <tim.d.song@gmail.com>
2018-10-09 17:22:48 -0700

    minor changes

--- db/migrate/20181010000553_create_users.rb
+++ db/migrate/20181010000553_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false
       t.string :email
       t.string :password_digest
       t.string :session_token
