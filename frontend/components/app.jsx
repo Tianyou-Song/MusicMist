c4c7129bbf1362260854bff0a7c266534553afc9
DawgSlaya <tim.d.song@gmail.com>
2018-10-10 11:29:42 -0700

    implement app component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App  = () => {
+  return (
+    <div>app</div>
+  );
+};
+
+export default App;
