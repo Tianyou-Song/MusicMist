52f5f047f7418009951eb7187eb00b3d06551a49
DawgSlaya <tim.d.song@gmail.com>
2018-10-10 09:32:05 -0700

    impliment route skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  # activerecord associations
+
+  def self.find_by_credentials(name, password)
+    user = User.find_by(name: name)
+    return nil unless uer
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,14 @@ ActiveRecord::Schema.define(version: 2018_10_10_000553) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "email"
-    t.string "password_digest"
-    t.string "session_token"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
